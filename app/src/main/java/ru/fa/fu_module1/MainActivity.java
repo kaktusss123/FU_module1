606df4b1208190ad1fe8359531899f737c263fca
Vladislav Komarnitsky <komarnitsky@ap-team.ru>
2021-04-11 13:52:06 +0300

    - Есть 3 кнопки: для инкремента, декремента и сброса счетчика - Нет возможности уйти в минус - Есть склонение в зависимости от числа кликов

--- app/src/main/java/ru/fa/fu_module1/MainActivity.java
+++ app/src/main/java/ru/fa/fu_module1/MainActivity.java
@@ -3,12 +3,42 @@ package ru.fa.fu_module1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button btnAdd;
+    Button btnSub;
+    Button btnDrop;
+    int clicks = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.infoTextView);
+        btnAdd = findViewById(R.id.buttonAdd);
+        btnSub = findViewById(R.id.buttonSub);
+        btnDrop = findViewById(R.id.buttonDrop);
+
+        refreshInfo(clicks);
+
+        btnAdd.setOnClickListener(v -> refreshInfo(clicks + 1));
+        btnSub.setOnClickListener(v -> refreshInfo(Math.max(clicks - 1, 0)));
+        btnDrop.setOnClickListener(v -> refreshInfo(0));
+    }
+
+    private void refreshInfo(int clicks) {
+        this.clicks = clicks;
+        textView.setText(String.format(getString(R.string.click_info) + (doWeNeedA(clicks) ? "а" : ""), clicks));
+    }
+
+    private boolean doWeNeedA(int number) {
+        return "234".contains(String.valueOf(number % 10)) && (number < 12 || number > 14);
     }
 }
