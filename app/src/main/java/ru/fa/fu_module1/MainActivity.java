7f91cf3317df00289f2016c207cd9dc3280386a2
Vladislav Komarnitsky <komarnitsky@ap-team.ru>
2021-04-11 13:24:01 +0300

    - Разметка меню читается из xml - Работает показ расширенного меню по `CheckBox` - Работает вывод информации о пункте меню - Работает выход по кнопке `exit`

--- app/src/main/java/ru/fa/fu_module1/MainActivity.java
+++ app/src/main/java/ru/fa/fu_module1/MainActivity.java
@@ -1,14 +1,52 @@
 package ru.fa.fu_module1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        checkBox = findViewById(R.id.chkbox);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.setGroupVisible(R.id.group1, checkBox.isChecked());
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_exit) {
+            finish();
+            System.exit(0);
+        }
+        String sb = "Item Menu" +
+                "\r\n groupId: " + item.getGroupId() +
+                "\r\n itemId: " + item.getItemId() +
+                "\r\n order: " + item.getOrder() +
+                "\r\n title: " + item.getTitle();
+        textView.setText(sb);
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
