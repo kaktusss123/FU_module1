e542b79a08febd140fe2648ba9e8492ba673e3ed
Vladislav Komarnitsky <komarnitsky@ap-team.ru>
2021-04-11 12:19:01 +0300

    - В `AppBar` моя фамилия - В `TextView` какой-то текст - Заменил на `LinearLayout` с ориентацией `vertical` - Присвоил `TextView` id и поменял при помощи Java

---
+++ app/Readme.md
@@ -0,0 +1,6 @@
+# MD0.1
+
+- В `AppBar` моя фамилия
+- В `TextView` какой-то текст
+- Заменил на `LinearLayout` с ориентацией `vertical`
+- Присвоил `TextView` id и поменял при помощи Java
--- app/src/main/java/ru/fa/fu_module1/MainActivity.java
+++ app/src/main/java/ru/fa/fu_module1/MainActivity.java
@@ -3,6 +3,7 @@ package ru.fa.fu_module1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.text1);
+        textView.setText(R.string.set_in_java);
     }
 }
